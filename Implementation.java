df3bb3596fc413ef56698551b17599ac05fcbc2e
NITHIS KUMAR S <105905948+nithis777@users.noreply.github.com>
2022-05-24 17:14:39 +0530

    Update Implementation.java

--- Implementation.java
+++ Implementation.java
@@ -208,11 +208,12 @@ public String replace(int a,char b){
 	   return last;
 	}
 public boolean contains(String str1) {
-	int n=str1.length();
+	char ch1[]=str1.toCharArray();
 	boolean flag=false;
-	for(int i=0;i<str.length();i++) {
-		if(i+n-1<str.length()&&str.substring(i,i+n).equals(str1)) {
-			flag=true;
+	for(int i=0;i<ch.length;i++) {
+		for(int j=0;j<ch1.length;j++) {
+		ch[i]=ch1[j];
+			flag=false;
 		}
 	}
 		return flag;
